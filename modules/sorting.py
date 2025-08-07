0a5c2edc2e89bbba2cbfd8c1eec2426fe73553e5
SiamXTest <sifamoni169@gmail.com>
2025-08-07 13:37:40 +0600

    Create sorting.py

---
+++ modules/sorting.py
@@ -0,0 +1,22 @@
+class Sort:
+    Sort_relevance = "relevance"
+    Sort_rating = "rating"
+    Sort_uploader = "uploader"
+    Sort_views = "views"
+
+class SortDate:
+    Sort_all = "all"
+    Sort_today = "t"
+    Sort_week = "w"
+    Sort_month = "m"
+    Sort_year = "y"
+
+class SortVideoTime:
+    Sort_all = "all"
+    Sort_short = "1"
+    Sort_medium = "2"
+    Sort_long = "3"
+
+class SortQuality:
+    Sort_all = "all"
+    Sort_hd = "hd"
